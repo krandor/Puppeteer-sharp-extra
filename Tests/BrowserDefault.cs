004bf8605c6c5fed7639b4441e450475ca947f68
krandor <krandor.delmarniol@gmail.com>
2024-06-14 11:21:09 -0400

    set headless back to true

--- Tests/BrowserDefault.cs
+++ Tests/BrowserDefault.cs
@@ -29,7 +29,7 @@ public abstract class BrowserDefault : IDisposable
     {
         return new LaunchOptions()
         {
-            Headless = false, // Constants.Headless,
+            Headless = Constants.Headless,
             ExecutablePath = browserPath,
         };
     }
